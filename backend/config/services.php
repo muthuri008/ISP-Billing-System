6ccb74c32c6e72254666153cb9e2102aba6b5efb
muthuri008 <jmuthuri210@gmail.com>
2026-08-16 15:16:35 +0300

    payments: add M-Pesa service configuration

---
+++ backend/config/services.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'mpesa' => [
+        'base_url' => env('MPESA_BASE_URL', 'https://sandbox.safaricom.co.ke'),
+        'consumer_key' => env('MPESA_CONSUMER_KEY'),
+        'consumer_secret' => env('MPESA_CONSUMER_SECRET'),
+        'shortcode' => env('MPESA_SHORTCODE'),
+        'passkey' => env('MPESA_PASSKEY'),
+        'callback_url' => env('MPESA_CALLBACK_URL'),
+    ],
+];
