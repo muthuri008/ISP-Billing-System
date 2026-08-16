eb940d33f7ea4c2b214e4c9470f07c6c2bea99be
TOJI <jmuthuri210@gmail.com>
2026-08-16 18:00:29 +0300

    frontend: route customer portal from application entrypoint

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import CustomerPortalApp from './CustomerPortalApp';
 import './styles.css';
+
+const isCustomerPortal = window.location.pathname.startsWith('/customer');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      {isCustomerPortal ? <CustomerPortalApp /> : <App />}
     </BrowserRouter>
   </StrictMode>,
 );
