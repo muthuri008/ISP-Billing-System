8e39c05936f714f590e77c8f4b268a9d3b7146e0
muthuri008 <jmuthuri210@gmail.com>
2026-08-16 14:58:37 +0300

    build: bootstrap Laravel application

---
+++ backend/bootstrap/app.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+        apiPrefix: 'api/v1',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        // Global middleware will be registered here as the application grows.
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        // API exception rendering will be standardised here.
+    })
+    ->create();
